d0c870a4ab262fb69c9a60926ae26f8890dc941f
pranav <prnvthir@gmail.com>
2021-03-30 13:15:23 -0400

    add graphql typeDefs and resolvers

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const messages =[];
+const messages = [];
 
-//define schema here
+//define schema here (type = messages)
+//define Query here (type = Query) - how we fetch data from GraphQL server
+//define Mutation here (type = Mutation) - how we add data to GraphQL server
 const typeDefs = `
 
   type Message {
@@ -15,27 +17,35 @@ const typeDefs = `
     messages:[Message!]
 
   }
+
+  type Mutation {
+  postMessage(user: String!, content: String!): ID!
+  }
+
 `;
 
 //typeDefs defines schema, resolvers are how we actually get the data back
 //resolvers match the keys that are in the typeDegs
 const resolvers = {
-
   Query: {
-    messages:()=>messages,
-  }
+    messages: () => messages,
+  },
 
-
-
-}
-
-
+  Mutation: {
+    postMessage: (parent, { user, content }) => {
+      const id = messages.length;
+      messages.push({
+        id,
+        user,
+        content,
+      });
+      return id;
+    },
+  },
+};
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 
-server.start(({port})=>{
-
-  console.log(`server started on port: ${port}`)
-
-
-})
+server.start(({ port }) => {
+  console.log(`server started on port: ${port}`);
+});
