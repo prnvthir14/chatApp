077fd2ac9021bb243abcffc823af9ecf2a5e39e4
pranav <prnvthir@gmail.com>
2021-03-31 11:01:54 -0400

    add UI for chat window and logic to render messages based on the current user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
+
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+import Chat from "./Chat";
+
+const App = () => (
+  <div>
+    <Chat />
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("app"));
